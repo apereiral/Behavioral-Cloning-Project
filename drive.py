49063de434dfec336f9c46238ea3ee660e692892
Lucas Pereira <lucaspereira0612@gmail.com>
2017-01-24 13:35:48 -0200

    changed preprocess pipeline from RGB to GRAYSCALE

--- drive.py
+++ drive.py
@@ -38,7 +38,9 @@ def telemetry(sid, data):
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
     image_array = np.asarray(image)
+    image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
     image_array = cv2.resize(image_array, (32, 32), interpolation=cv2.INTER_AREA)
+    image_array = image_array.reshape((32, 32, 1))
     image_array = image_array/255. - 0.5
     transformed_image_array = image_array[None, :, :, :]
     # This model currently assumes that the features of the model are just the images. Feel free to change this.
