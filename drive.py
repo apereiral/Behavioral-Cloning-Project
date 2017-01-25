7d8bac1267c75a4d3308690d399e0fcb7db3d55b
Lucas Pereira <lucaspereira0612@gmail.com>
2017-01-24 23:01:05 -0200

    added cropping to the preprocess pipeline - best result so far

--- drive.py
+++ drive.py
@@ -39,7 +39,7 @@ def telemetry(sid, data):
     image = Image.open(BytesIO(base64.b64decode(imgString)))
     image_array = np.asarray(image)
     image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
-    image_array = cv2.resize(image_array, (32, 32), interpolation=cv2.INTER_AREA)
+    image_array = cv2.resize(image_array[60:135, 50:270], (32, 32), interpolation=cv2.INTER_AREA)
     image_array = image_array.reshape((32, 32, 1))
     image_array = image_array/255. - 0.5
     transformed_image_array = image_array[None, :, :, :]
